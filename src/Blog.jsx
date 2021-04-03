2974b8e178cf24272c4c2a1435d90cf6a09cf6b6
masakifujie <yan.yan.yan@icloud.com>
2021-04-04 08:17:33 +0900

    firstAdd

---
+++ src/Blog.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Article from './Article';
+
+class Blog extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            isPublished: false,
+            count: 0
+        }
+    }
+
+    componentDidMount() {
+        //ボタンがクリックされたらいいねをカウントアップする
+        document.getElementById('counter').addEventListener('click', this.countUp)
+    }
+
+    componentDidUpdate() {
+        if (this.state.count >= 10) {
+            this.setState({count: 0 })
+        }
+    }
+
+    componentWillUnmount() {
+        document.getElementById('counter').removeEventListener('click',this.countUp)
+    }
+
+    //公開状態を反転させる
+    togglePublished = () => {
+        this.setState({
+            isPublished: !this.state.isPublished
+        })
+    };
+
+    countUp = () => {
+        this.setState({count:this.state.count + 1})
+    }
+
+    render() {
+        return (
+            <>
+                <Article
+                    tittle={"Reactの使い方"}
+                    isPublished={this.state.isPublished}
+                    toggle={() => this.togglePublished()}
+                    count={this.state.count} />
+            </>
+        )
+    }
+}
+
+export default Blog
