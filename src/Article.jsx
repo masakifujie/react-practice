b1ebfb828a2d8411795bf5379feebbedf9f2dbe1
masakifujie <yan.yan.yan@icloud.com>
2021-04-04 12:33:44 +0900

    changedToReacthooks

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,19 +1,15 @@
-import React from 'react'
-import LikeButton from "./LikeButton";
+import React,{useState} from 'react'
+// import LikeButton from "./LikeButton";
 
 const Article = (props) => {
-    let publishState = "";
-    if (props.isPublished) {
-        publishState = "公開"
-    } else {
-        publishState = "非公開"
-    }
+    const [isPublished, togglePublished] = useState(false);
+    
     return (
         <div>
             <h2>{props.tittle}</h2>
             <label htmlFor="check">公開状態:</label>
-            <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()} />
-            <LikeButton count={props.count}/>
+            <input type="checkbox" checked={isPublished} id="check" onClick={() => togglePublished(!isPublished)} />
+            {/* <LikeButton count={props.count}/> */}
         </div>
     )
 } 
